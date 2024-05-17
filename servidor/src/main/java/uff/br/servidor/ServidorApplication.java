46eed9806c98a40a41d8ddb56f2792e38ccc457e
rafabpg <rafa23giordano@gmail.com>
2024-05-17 20:22:28 -0300

    feat: adding servidor project

--- servidor/src/main/java/uff/br/servidor/ServidorApplication.java
+++ servidor/src/main/java/uff/br/servidor/ServidorApplication.java
@@ -1,13 +1,13 @@
-package uff.com.server;
+package uff.br.servidor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ServidorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(ServidorApplication.class, args);
 	}
 
 }
--- servidor/src/test/java/uff/br/servidor/ServidorApplicationTests.java
+++ servidor/src/test/java/uff/br/servidor/ServidorApplicationTests.java
@@ -1,10 +1,10 @@
-package uff.com.server;
+package uff.br.servidor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class ServidorApplicationTests {
 
 	@Test
 	void contextLoads() {
