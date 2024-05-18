4ccd1fd2cabf45c8cb23524ef1292d1ec87d1a5b
gustavo-ferreira03 <gustavo_cosme@id.uff.br>
2024-05-18 18:10:35 -0300

    feat(client): add header

--- cliente/src/App.tsx
+++ cliente/src/App.tsx
@@ -1,10 +1,11 @@
 import { Button } from './components/ui/button'
+import { HomePage } from './pages/HomePage'
 
 function App() {
 
   return (
     <>
-    <Button>teste</Button>
+      <HomePage/>
     </>
   )
 }
