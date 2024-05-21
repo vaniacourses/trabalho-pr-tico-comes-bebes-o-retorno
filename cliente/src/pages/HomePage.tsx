d88e95dfe13a407972432cb44af2195ea786671c
gustavo-ferreira03 <gustavo_cosme@id.uff.br>
2024-05-21 19:10:44 -0300

    feat(client): troca o tema da pagina

--- cliente/src/pages/HomePage.tsx
+++ cliente/src/pages/HomePage.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 
 export function HomePage() {
   return (
-    <header className="flex items-center bg-black justify-between px-24 py-8">
-      <h1 className="font-bold text-xl text-white">Comes & Bebes</h1>
+    <header className="flex items-center justify-between px-24 py-8 shadow">
+      <h1 className="text-xl">Comes & Bebes</h1>
       <nav>
-        <div>
+        <div className="flex">
           {/* <Button variant="ghost">criar conta</Button> */}
+          <Button variant="link" className="text-black">criar conta</Button>
           <Button>Entrar</Button>
         </div>
       </nav>
