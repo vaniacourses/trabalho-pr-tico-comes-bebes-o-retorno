f92c9468c6e551bfad55efdf7c27104ba1be7321
rafabpg <rafa23giordano@gmail.com>
2024-06-29 12:37:35 -0300

    feat: adding response of token, taking off expire date and

--- servidor/src/main/java/uff/br/servidor/security/AuthUserService.java
+++ servidor/src/main/java/uff/br/servidor/security/AuthUserService.java
@@ -25,7 +25,7 @@ public class AuthUserService {
     private PasswordEncoder passwordEncoder;
 
 
-    public String login(AuthUserDTO authUserDTO) {
+    public AuthUserResponseDTO login(AuthUserDTO authUserDTO) {
         var usuario = this.usuarioRepository.findByCpf(authUserDTO.getCpf()).orElse(null);
         if(usuario == null){
             throw new RuntimeException("Cpf nao encontrado");
@@ -33,9 +33,9 @@ public class AuthUserService {
             var password = this.passwordEncoder.matches(authUserDTO.getSenha_hash(), usuario.getSenha_hash());
             if(password){
                 Algorithm algorithm = Algorithm.HMAC256(secret_key);
-                var token = JWT.create().withIssuer("secret").withExpiresAt(Instant.now().plus(Duration.ofHours(2))).withSubject(usuario.getId().toString()).sign(algorithm);
-
-                return token;   
+                var token = JWT.create().withIssuer("secret").withSubject(usuario.getId().toString()).sign(algorithm);
+                var auth = AuthUserResponseDTO.builder().token(token).role(usuario.getRole()).build();
+                return auth;   
             }else{
                 throw new RuntimeException("Senha invalida");
             }
