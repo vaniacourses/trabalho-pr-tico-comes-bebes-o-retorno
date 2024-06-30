6b0fdb72dd10c1cfc4970eaf0ad11d46929e72fe
rafabpg <rafa23giordano@gmail.com>
2024-06-30 14:51:37 -0300

    fix:fixing header

--- cliente/src/components/Header.tsx
+++ cliente/src/components/Header.tsx
@@ -5,9 +5,10 @@ import { useContext } from "react";
 
 export function Header(){
   
-    const { isAuthenticated} = useContext(AuthContext)
+    const { isAuthenticated,setIsAuthenticated} = useContext(AuthContext)
     const handleLogout = () =>{
         localStorage.removeItem("acess-token")
+        setIsAuthenticated(false)
         sessionStorage.clear();
     }
     return(
@@ -20,7 +21,7 @@ export function Header(){
               <Link to ={"/cadastro"}><Button>Criar conta</Button></Link>
               <Link to ={"/login"}><Button>Entrar</Button></Link>
             </>):(
-              <Button onClick={handleLogout} ><Link to="/">Logout</Link></Button>
+              <Button onClick={handleLogout} ><Link to="/home">Logout</Link></Button>
               
             )
           }</div>
